6eba9800a27b2322f00dab1506be61c03d41c9a4
natyudon <61405828+natyudon@users.noreply.github.com>
2021-08-04 23:36:25 +0900

    選択されたカードの処理追加

--- Assets/Scripts/HandCardScript.cs
+++ Assets/Scripts/HandCardScript.cs
@@ -10,24 +10,47 @@ public class HandCardScript : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData pointerData)
     {
-        //セレクトカードマネージャーに選択したカードをセット
-        SelectHandManager.SetSelectCard(cardName);
-
         var x = this.transform.localPosition.x;
         var y = this.transform.localPosition.y;
         var z = this.transform.localPosition.y;
         if (y == 0)
         {
-            this.transform.localPosition = new Vector3(x, y + 25, z);
+            //セレクトカードマネージャーに選択したカードをセット
+            SelectHandManager.SetSelectCard(cardName);
 
+            this.transform.localPosition = new Vector3(x, y + 25, z);
             this.GetComponent<Outline>().effectColor = new Color(255f / 255f, 134f / 255f, 0f, 1f);
             this.GetComponent<Outline>().effectDistance = new Vector2(3f, 5f);
         }
         else
         {
+            //セレクトカードマネージャーに選択したカードをセット
+            SelectHandManager.ResetSelectCard(cardName);
+
             this.transform.localPosition = new Vector3(x, y - 25, z);
             this.GetComponent<Outline>().effectColor = new Color(0f, 0f, 0f, 0.5f);
             this.GetComponent<Outline>().effectDistance = new Vector2(1f, 1f);
         }
+
+        /*var cardLen = cardName.Length;
+        string lastStr;
+        if (cardName != "Joker_Color")
+        {
+            lastStr = cardName.Substring(cardLen - 2);
+        }
+        
+        var selectcards = SelectHandManager.selectCards;
+        if(selectcards.Count == 0)
+        {
+            
+        }
+        else if (selectcards.Count == 1)
+        {
+            if(cardName == "Joker_Color")
+            {
+                SelectHandManager.SetSelectCard(cardName);
+            }
+            selectcards[0].Contains(lastStr);
+        }*/
     }
 }
--- Assets/Scripts/SelectHandManager.cs
+++ Assets/Scripts/SelectHandManager.cs
@@ -9,18 +9,44 @@ public class SelectHandManager : MonoBehaviour
     private void Awake()
     {
         var PlayeyHand = GameObject.Find("PlayerHandPanel(Clone)").GetComponent<PlayerObject>().playerModel.hands;
-        foreach (var hand in PlayeyHand)
-        {
-            Debug.Log(hand);
-        }
     }
 
     public static void SetSelectCard(string card)
     {
+        //仕様変更にて一旦この処理コメントアウト
+        /*if(selectCards.Count != 0)
+        {
+            var laststr = LastStr(card);
+            switch (selectCards.Count)
+            {
+                //選択されているカードが一枚の時
+                case 1:
+                    if(!selectCards[0].Contains(laststr))
+                    {
+                        ResetSelectCard(selectCards[0]);
+                    }
+                    break;
+            }
+        }*/
+
         selectCards.Add(card);
         foreach(var k in selectCards)
         {
             Debug.Log(k);
         }
+    }
+
+    public static void ResetSelectCard(string card)
+    {
+        selectCards.Remove(card);
+        foreach(var k in selectCards)
+        {
+            Debug.Log(k);
+        }
+    }
+
+    public static string LastStr(string str)
+    {
+        return str.Substring(str.Length - 2);
     }
 }
