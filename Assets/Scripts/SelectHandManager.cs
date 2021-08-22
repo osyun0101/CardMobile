bdf7f6d0ba13646644b8f1edb5f6764705ec0993
natyudon <61405828+natyudon@users.noreply.github.com>
2021-08-22 13:03:36 +0900

    連番が選ばれた時の処理追加

--- Assets/Scripts/SelectHandManager.cs
+++ Assets/Scripts/SelectHandManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SelectHandManager : MonoBehaviour
 {
     public static List<HandCardScript> selectCards = new List<HandCardScript>();
+    public static bool serial = false;
 
     private void Awake()
     {
@@ -14,33 +15,51 @@ public class SelectHandManager : MonoBehaviour
 
     public static void SetSelectCard(HandCardScript card)
     {
-        if(selectCards.Count != 0)
+        if (selectCards.Count == 1)
         {
-            var laststr = LastStr(card.cardName);
-            var lastStrCheck = true;
-            foreach(var c in selectCards)
+            int index_0;
+            int cardint;
+            var ParseBool = int.TryParse(LastStr(selectCards[0].cardName), out index_0);
+            var ParseBool2 = int.TryParse(LastStr(card.cardName), out cardint);
+            if(cardint + 1 == index_0 || cardint - 1 == index_0)
             {
-                if (!c.cardName.Contains(laststr))
+                if (ParseBool && ParseBool2)
                 {
-                    lastStrCheck = false;
-                    break;
+                    serial = true;
                 }
-            }
-            if (!lastStrCheck)
-            {
-                foreach(var c in selectCards)
-                {
-                    var x = c.transform.localPosition.x;
-                    var y = c.transform.localPosition.y;
-                    var z = c.transform.localPosition.z;
-                    c.transform.localPosition = new Vector3(x, y - 25, z);
-                    c.GetComponent<Outline>().effectColor = new Color(0f, 0f, 0f, 0.5f);
-                    c.GetComponent<Outline>().effectDistance = new Vector2(1f, 1f);
-                }
-                selectCards = new List<HandCardScript>();
             }
         }
 
+        if (selectCards.Count != 0)
+        {
+            if (!serial)
+            {
+                var laststr = LastStr(card.cardName);
+                var lastStrCheck = true;
+                foreach (var c in selectCards)
+                {
+                    if (!c.cardName.Contains(laststr))
+                    {
+                        lastStrCheck = false;
+                        break;
+                    }
+                }
+                if (!lastStrCheck)
+                {
+                    foreach (var c in selectCards)
+                    {
+                        var x = c.transform.localPosition.x;
+                        var y = c.transform.localPosition.y;
+                        var z = c.transform.localPosition.z;
+                        c.transform.localPosition = new Vector3(x, y - 25, z);
+                        c.GetComponent<Outline>().effectColor = new Color(0f, 0f, 0f, 0.5f);
+                        c.GetComponent<Outline>().effectDistance = new Vector2(1f, 1f);
+                    }
+                    selectCards = new List<HandCardScript>();
+                }
+            }
+        }
+        serial = false;
         selectCards.Add(card);
     }
 
